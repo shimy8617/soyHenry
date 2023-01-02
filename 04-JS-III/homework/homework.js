30ddf198a6dde277ddd2998f9ff469cb1db1bc3a
shimy8617 <cristinashim@yahoo.com.ar>
2023-01-02 11:41:10 -0300

    jsiii

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -122,6 +122,17 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  multiplicacion = arguments[0];
+  if(arguments.length !== 0) {
+    for(var i = 0; i < arguments.length -1; i++) {
+      multiplicacion = multiplicacion * arguments[i] 
+  }
+  
+} else {
+  return 0
+  
+  }
+  return multiplicacion
 }
 
 
