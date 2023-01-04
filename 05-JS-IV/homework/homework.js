254e59338bc6d700da8592729a37e733658b6754
shimy8617 <cristinashim@yahoo.com.ar>
2023-01-04 16:23:01 -0300

    js iv

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,13 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  let gato = {
+    nombre : nombre,
+    edad: edad,
+    meow: function(){console.log("Meow") 
+  }
+  }
+  return gato
 }
 
 function agregarPropiedad (objeto, property) {
